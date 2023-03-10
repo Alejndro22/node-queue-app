440f5f83c127515f20048318274e795be3f57043
Alejndro22 <38710085+Alejndro22@users.noreply.github.com>
2023-03-10 01:16:57 -0600

    create ticket using socket events, and display last ticket number

--- public/js/nuevo-ticket.js
+++ public/js/nuevo-ticket.js
@@ -1,3 +1,28 @@
+// Referencias del HTML
+const lblNewTicket = document.querySelector('#lblNuevoTicket');
+const btnCreate = document.querySelector('button');
 
+const socket = io();
 
-console.log('Nuevo Ticket HTML');
+socket.on('connect', () => {
+  // console.log('Conectado');
+
+  btnCreate.disabled = false;
+});
+
+socket.on('disconnect', () => {
+  // console.log('Desconectado del servidor');
+
+  btnCreate.disabled = true;
+});
+
+socket.on('last-ticket', (payload) => {
+  // used to display last ticket for client
+  lblNewTicket.innerText = 'Ticket ' + payload;
+});
+
+btnCreate.addEventListener('click', () => {
+  socket.emit('create-ticket', null, (ticket) => {
+    lblNewTicket.innerText = ticket;
+  });
+});
--- sockets/controller.js
+++ sockets/controller.js
@@ -3,19 +3,15 @@ import TicketControl from '../models/ticket-control.js';
 const ticketControl = new TicketControl();
 
 const socketController = (socket) => {
-  // 1st arg on callback is the receibed obj
-  // 2nd arg on callback is a ref to client function, only sends to client who sent
-  socket.on('send-msg', (payload, callback) => {
+  socket.emit('last-ticket', ticketControl.last);
+
+  socket.on('create-ticket', (payload, callback) => {
     if (!callback) return;
 
-    const id = 123456;
-    callback({
-      id,
-      date: new Date().getTime(),
-    });
+    const nextTicket = ticketControl.nextTicket();
+    callback(nextTicket);
 
-    // This is used to send a broadcast msg (anyone listening)
-    socket.broadcast.emit('msg-sent', payload);
+    // TODO: Notify there's a new ticket waiting to be assigned
   });
 };
 
